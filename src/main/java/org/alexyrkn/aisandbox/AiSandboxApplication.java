b2ad71ee1813c39aa7c6ca12ffe9aeee70b2a195
Aleksandr Yurkin <ayurkin@paladin-group.ru>
2026-05-25 20:53:57 +0300

    Initial commit

---
+++ src/main/java/org/alexyrkn/aisandbox/AiSandboxApplication.java
@@ -0,0 +1,13 @@
+package org.alexyrkn.aisandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/alexyrkn/aisandbox/AiSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alexyrkn.aisandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
