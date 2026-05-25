4324621952a9ba84cccc7f47000d88a33ad78261
Aleksandr Yurkin <ayurkin@paladin-group.ru>
2026-05-25 22:24:30 +0300

    Added base of working with LLM

--- src/main/java/org/alexyrkn/aisandbox/AiSandboxApplication.java
+++ src/main/java/org/alexyrkn/aisandbox/AiSandboxApplication.java
@@ -1,13 +1,23 @@
 package org.alexyrkn.aisandbox;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AiSandboxApplication {
 
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder) {
+        return builder.build();
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(AiSandboxApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(AiSandboxApplication.class, args);
+        ChatClient chatClient = context.getBean(ChatClient.class);
+        System.out.println(chatClient.prompt().user("Коротко что такое RAG?").call().content());
     }
 
 }
